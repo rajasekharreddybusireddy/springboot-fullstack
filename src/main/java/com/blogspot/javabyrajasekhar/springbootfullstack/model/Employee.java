1f22fbecf90104c408236a298f06bb1d809a75bf
rajasekharreddybusireddy <rajasekharreddy.busi@gmail.com>
2020-03-11 08:56:38 +0530

    validations code fix

--- src/main/java/com/blogspot/javabyrajasekhar/springbootfullstack/model/Employee.java
+++ src/main/java/com/blogspot/javabyrajasekhar/springbootfullstack/model/Employee.java
@@ -1,14 +1,13 @@
 package com.blogspot.javabyrajasekhar.springbootfullstack.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-
-import org.hibernate.validator.constraints.NotEmpty;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,17 +22,13 @@ public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	@NotBlank
-	 @Column(name="first_name")
+	@NotBlank(message = "First Name must be supplied")
+	@Size(min = 2,max = 30)
 	private String firstName;
-	@NotBlank
-	 @Column(name="last_name")
+	@NotBlank(message = "Last Name must be supplied")
+	@Size(min = 2,max = 30)
 	private String lastName;
 	@Email
-	 @Column(name="email", nullable=false, length=200)
+	@NotEmpty(message = "please provide Email")
 	private String email;
-
-	// Setters and getters
-
-	
 }
